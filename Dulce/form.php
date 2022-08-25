7aa3c87b4cf3906871a8aff346b0403b7b77148d
Dulcemartin <92184604+Dulcemartin@users.noreply.github.com>
2022-08-25 17:31:10 -0600

    worked on contact form

--- Dulce/form.php
+++ Dulce/form.php
@@ -19,6 +19,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <link rel="stylesheet" href="contact.css">
+    <style>
+    body{
+      background-image: url("/Dulce/Assets/pexels-olha-ruskykh-6954558.jpg");
+      color:white;
+    }
+
+
+
+    </style>
     </head>
 
     <!-- BODY -->
